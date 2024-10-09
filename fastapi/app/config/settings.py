5d330dbdbf5322dcd345164ea25f2eaf95a6bf3e
blandoncj <jacoboblandon94@gmail.com>
2024-10-09 11:29:35 -0500

    feat: add settings for application configuration

---
+++ fastapi/app/config/settings.py
@@ -0,0 +1,29 @@
+"""
+This file contains the settings for the application.
+"""
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ENV = os.getenv("ENV", "dev")
+
+if ENV == "production":
+    DATABASE = {
+        "name": os.getenv("MYSQL_DATABASE"),
+        "engine": "peewee.MySQLDatabase",
+        "user": os.getenv("MYSQL_USER"),
+        "password": os.getenv("MYSQL_PASSWORD"),
+        "host": os.getenv("MYSQL_HOST"),
+        "port": int(os.getenv("MYSQL_PORT")),
+    }
+else:
+    DATABASE = {
+        "name": os.getenv("MYSQL_DATABASE"),
+        "engine": "peewee.MySQLDatabase",
+        "user": os.getenv("MYSQL_USER"),
+        "password": os.getenv("MYSQL_PASSWORD"),
+        "host": os.getenv("MYSQL_HOST"),
+        "port": int(os.getenv("MYSQL_PORT")),
+    }
